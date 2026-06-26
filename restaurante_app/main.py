5d876b2b3b7f5f477830ada6852f0b77a29502d3
Nicolas Galarraga <nicolasgalarraga1095@gmail.com>
2026-06-26 16:46:18 -0500

    Add files via upload

--- restaurante_app/main.py
+++ restaurante_app/main.py
@@ -9,3 +9,13 @@ Cliente1 = Cliente("Juan", "123456789")
 Cliente2 = Cliente("Maria", "987654321")
 
 restaurante = Restaurante("Mi Restaurante")
+
+restaurante.agregar_producto(Producto1)
+restaurante.agregar_producto(Producto2)
+
+restaurante.registrar_cliente(Cliente1)
+restaurante.registrar_cliente(Cliente2)
+
+restaurante.mostrar_menu()
+
+restaurante.mostrar_clientes()
