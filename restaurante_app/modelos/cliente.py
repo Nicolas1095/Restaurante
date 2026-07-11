f848cec22040dfe1908b9d857850e0f1a7d7c230
Nicolas Galarraga <nicolasgalarraga1095@gmail.com>
2026-07-11 11:45:38 -0500

    Delete restaurante_app/modelos/cliente.py

--- restaurante_app/modelos/cliente.py
+++
@@ -1,8 +0,0 @@
-class Cliente:
-    # Crea la clase cliente recibiendo el nombre y telefono como parametro
-    def __init__(self, nombre: str, telefono: str):
-        self.nombre = nombre
-        self.telefono = telefono
-    # Cuando se imprima un cliente, se mostrará como string el nombre y el teléfono del cliente
-    def __str__(self) -> str:
-        return f"{self.nombre} - {self.telefono}"
